b24fd09a975ec964ed2182843dcaa51911de4f66
Matthew <matt-hope@hotmail.com>
2020-10-17 20:58:15 -0400

    added target blank to links/ started email copy function

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,10 @@
 console.log("scriptsss");
 console.log("Hellooo");
 
+function copyEmail() {
+    const textToCopy = "matt-hope@outlook.com";
 
+    document.execCommand("Copy");
 
-
+    console.log("Copied");
+}
