a7cab89cea73314080b20e04fe940f07761826a4
Matthew <matt-hope@hotmail.com>
2020-10-17 22:00:00 -0400

    Added copy email function

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 
-console.log("scriptsss");
-console.log("Hellooo");
+const app = {}
 
-function copyEmail() {
+// https://codepen.io/eclarrrk/pen/ZZywZv
+
+app.copyEmail = function() {
     const textToCopy = "matt-hope@outlook.com";
-
+    const placeholder = document.createElement("input");
+    document.body.appendChild(placeholder);
+    placeholder.setAttribute('value', textToCopy);
+    placeholder.select();
     document.execCommand("Copy");
-
     console.log("Copied");
+    document.body.removeChild(placeholder);
 }
