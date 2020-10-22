cc4acdb14a5a10e7904b8f22434238f2a32cfb22
Matthew <matt-hope@hotmail.com>
2020-10-21 22:00:13 -0400

    continued working on email copy button

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,26 @@ app.copyEmail = function() {
     console.log("Copied");
     document.body.removeChild(placeholder);
 }
+
+const button = document.getElementById('copy-email-icon');
+
+button.addEventListener('click', () =>{
+    // app.copyEmail();
+    myFunction();
+});
+
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("myInput");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+  
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+}
