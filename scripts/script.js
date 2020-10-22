9c4a690fc4cee95fdbf4668fd944128944141650
Matthew <matt-hope@hotmail.com>
2020-10-22 07:46:35 -0400

    completed copy email function

--- scripts/script.js
+++ scripts/script.js
@@ -10,29 +10,13 @@ app.copyEmail = function() {
     placeholder.setAttribute('value', textToCopy);
     placeholder.select();
     document.execCommand("Copy");
-    console.log("Copied");
     document.body.removeChild(placeholder);
 }
 
-const button = document.getElementById('copy-email-icon');
+const copyEmailButton = document.getElementById('copy-email-icon');
 
-button.addEventListener('click', () =>{
-    // app.copyEmail();
-    myFunction();
+copyEmailButton.addEventListener('click', () =>{
+    app.copyEmail();
 });
 
 
-function myFunction() {
-    /* Get the text field */
-    var copyText = document.getElementById("myInput");
-  
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
-  
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-  
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
-}
